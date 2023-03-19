a9da636ab3ed765a3978dd10218bd094679bb99e
Sjaak van den Brom <12882171+sjaakvdbrom@users.noreply.github.com>
2023-03-19 23:22:52 +0100

    Start adding the new content and style cards a bit

--- components/Card.js
+++ components/Card.js
@@ -1,9 +1,10 @@
 import styles from '../styles/Card.module.scss'
 
-export default function Card({ title, completed }) {
+export default function Card({ title, completed, description }) {
     return (
       <div className={`${styles.container} ${completed ? styles.completed : styles.uncompleted}`}>
-        {title}
+        <div className={styles.title}>{title}</div>
+        <div className={styles.description}>{description}</div>
       </div>
     )
   }
