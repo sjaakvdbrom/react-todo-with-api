d3e4374fe834f2da7e9c6b38175a31b09346c9a8
Sjaak van den Brom <12882171+sjaakvdbrom@users.noreply.github.com>
2023-03-20 12:33:32 +0100

    Start working on dates

--- components/LargeCard.js
+++ components/LargeCard.js
@@ -1,10 +1,13 @@
 import styles from '../styles/LargeCard.module.scss'
 import typo from '../styles/Typography.module.scss'
+import { format, parseISO } from 'date-fns'
 
 export default function LargeCard({ title, categoryId, categories, date, description }) {
     const getCategory = (array, id) => {
         return array.find((element) => element.id === id)
     }
+
+    console.log(parseISO("2022-07-23"))
 
     return (  
       <div className={styles.container} style={{'--color-category': getCategory(categories, categoryId).color}}>
