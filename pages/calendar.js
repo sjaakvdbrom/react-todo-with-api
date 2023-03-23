746780d4bc3a6397ea031baa95870d3f1333a71a
Sjaak van den Brom <12882171+sjaakvdbrom@users.noreply.github.com>
2023-03-23 05:58:01 +0100

    Start working on dayboxes

--- pages/calendar.js
+++ pages/calendar.js
@@ -4,6 +4,7 @@ import { HiOutlineChevronLeft } from 'react-icons/hi';
 import { TbDotsVertical } from 'react-icons/tb';
 import button from '../styles/Buttons.module.scss'
 import typo from '../styles/Typography.module.scss'
+import styles from '../styles/Calendar.module.scss'
 
 export default function Calendar() {
     return (
@@ -16,7 +17,10 @@ export default function Calendar() {
                         <button className={button.icon} title='Go back'><TbDotsVertical /></button>
                     </header>
                     <main>
-                        Test
+                        <div className={styles.dayBox}>
+                            <div className={styles.number}>08</div>
+                            <div className={styles.day}>Sat</div>
+                        </div>
                     </main>
                 </div>
             </div>
